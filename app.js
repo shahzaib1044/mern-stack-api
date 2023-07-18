73fce380f9b003d13df3f06205396eb9e18c7334
shahzaib1044 <127635002+shahzaib1044@users.noreply.github.com>
2023-07-18 19:10:06 +0500

    Update app.js

--- app.js
+++ app.js
@@ -2,9 +2,8 @@ const express = require('express');
 const app = express();
 const productRoute = require('./api/routes/product');
 const userRoute = require('./api/routes/user');
-const categorypath = require('./api/routes/category')
+const categoryPath = require('./api/routes/category');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 const { urlencoded, json } = require('body-parser');
 const cors = require('cors');
 const fileUpload = require('express-fileupload');
@@ -17,25 +16,29 @@ mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
   })
   .catch((error) => {
     console.error('Error connecting to MongoDB:', error);
-  }); 
+  });
 
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(bodyParser.json());
-
+app.use(urlencoded({ extended: false }));
+app.use(json());
 app.use(fileUpload({
-  useTempFiles:true
-}))
+  useTempFiles: true
+}));
 
 app.use(cors());
 
-app.use('/product',productRoute);
-app.use('/user',userRoute);
-app.use('/category',categorypath);
+app.use('/product', productRoute);
+app.use('/user', userRoute);
+app.use('/category', categoryPath);
 
-app.get('*',(req,res,next)=>{
-  res.status(200).json({
-    message:'bad request'
-  })
-})
+app.get('*', (req, res, next) => {
+  res.status(404).json({
+    message: 'Bad request'
+  });
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: 'Server error' });
+});
 
 module.exports = app;
