7bac448f8fb0dbebc649fd21cff53b754fdec8bf
shahzaib1044 <127635002+shahzaib1044@users.noreply.github.com>
2023-07-18 20:21:34 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
-const app = express();
+
+const http = require('http');
+const app = require('./app');
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 require("dotenv").config();
